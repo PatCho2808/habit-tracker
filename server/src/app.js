fb40116d67fbe0afbb77d59fb37a17609ecff9e4
Patrycja <patcho2808@gmail.com>
2020-09-08 13:19:00 +0200

    Added error handlers as middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
 const app = express();
+const middlewares = require('./middlewares'); 
 
 app.use('/', (req, res) => {
     res.end('hello');
-})
+}); 
+
+app.use(middlewares.notFound); 
+app.use(middlewares.errorHandler); 
 
 module.exports = app; 
