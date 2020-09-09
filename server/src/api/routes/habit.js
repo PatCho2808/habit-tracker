14375eaaf6ee69b2e8a6aa731556807cc65c92b4
Patrycja <patcho2808@gmail.com>
2020-09-09 14:09:57 +0200

    Added authenticate token

--- server/src/api/routes/habit.js
+++ server/src/api/routes/habit.js
@@ -1,5 +1,8 @@
 const router = require('express').Router(); 
 const controller = require('../controllers/habitController'); 
+const middlewares = require('../../middlewares'); 
+
+router.use(middlewares.authenticateToken); 
 
 router.get('/all', controller.getAllHabits);
 router.post('/create', controller.createHabit); 
--- server/src/middlewares.js
+++ server/src/middlewares.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken'); 
+const { response } = require('./app');
+
 const notFound = (req, res, next) => {
     const error = new Error(`Not found = ${req.originalUrl}`); 
     res.status(404); 
@@ -13,7 +16,26 @@ const errorHandler = (err, req, res, next) => {
     }); 
 }; 
 
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']; 
+    const token = authHeader && authHeader.split(' ')[1]; 
+    if(!token) {
+        res.status(401); 
+        next(new Error("Token cannot be empty")) 
+    }
+    jwt.verify(token, process.env.API_SECRET, (err, user) => {
+        if(err) {
+            res.status(403); 
+            next(err);  
+        }
+        console.log(user); 
+        req.user = user; 
+        next(); 
+    })
+}; 
+
 module.exports = {
     notFound,
-    errorHandler
+    errorHandler, 
+    authenticateToken
 }
