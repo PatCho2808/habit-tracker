33e33cbf239b71d101782f21585a3d8df8fb9439
Patrycja <patcho2808@gmail.com>
2020-09-10 10:19:16 +0200

    Added min and max for username and password

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -5,11 +5,14 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true, 
+        minlength: 3, 
+        maxlength: 30
     },
     password: {
         type: String,
-        required: true
+        required: true, 
+        minlength: 6
     }
 }, { timestamps: true });
 
