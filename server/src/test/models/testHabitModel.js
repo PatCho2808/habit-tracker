d8d453ae0366e200fc90ca08f37336961e31fb39
Patrycja <patcho2808@gmail.com>
2020-09-26 11:25:04 +0200

    Added test for upddating streak - mon and wed

--- server/src/test/models/testHabitModel.js
+++ server/src/test/models/testHabitModel.js
@@ -7,6 +7,7 @@ const Habit = require('../../models/habit');
 const ObjectId = require('mongoose').Types.ObjectId;
 const db = require('mongoose');
 const { db_url } = require('../../config');
+const { update } = require('../../models/habit');
 
 chai.use(dateTime);
 
@@ -14,42 +15,78 @@ const userId = '5f65ec1e364ff1431fdd6a62';
 
 describe('habit model', function () {
 
-    beforeEach(function () {
-        let startDate = new Date();
-        startDate.setDate(startDate.getDate() - 5);
-        const habit = new Habit({
-            "name": "test",
-            "weekdays": [0, 1, 2, 3, 4, 5, 6],
-            "userId": new ObjectId(userId),
-            "startDate": startDate
-        });
-        this.currentTest.habit = habit;
-    });
+    describe('habit with every day in weekday', function () {
 
-    describe('add done at', function () {
-        it('should add date with zero time after midnight', function () {
-            const date = new Date('Thu Sep 24 2020 14:45:35 GMT+0200');
-            const zeroDate = new Date('Thu Sep 24 2020 00:00:00');
-            this.test.habit.addDoneAt(date);
-            this.test.habit.doneAtDates[0].should.be.equalTime(zeroDate);
-        });
-    });
-
-    describe('update streaks', function () {
-
-        it('should update current and longest streak to 4', function () {
-            const dates = []; 
-            for(let i=0; i<4; i++){
-                let newDate = new Date(); 
-                newDate.setDate(newDate.getDate() - i); 
-                dates.push(newDate); 
-            }
-            dates.forEach(date => this.test.habit.addDoneAt(date)); 
-            this.test.habit.updateStreaks();
-            this.test.habit.currentStreak.should.be.equal(4);
-            this.test.habit.longestStreak.should.be.equal(4);
+        beforeEach(function () {
+            let startDate = new Date();
+            startDate.setDate(startDate.getDate() - 5);
+            const habit = new Habit({
+                "name": "test",
+                "weekdays": [0, 1, 2, 3, 4, 5, 6],
+                "userId": new ObjectId(userId),
+                "startDate": startDate
+            });
+            this.currentTest.habit = habit;
         });
 
+        describe('add done at', function () {
+            it('should add date with zero time after midnight', function () {
+                const date = new Date('Thu Sep 24 2020 14:45:35 GMT+0200');
+                const zeroDate = new Date('Thu Sep 24 2020 00:00:00');
+                this.test.habit.addDoneAt(date);
+                this.test.habit.doneAtDates[0].should.be.equalTime(zeroDate);
+            });
+        });
+
+        describe('update streaks', function () {
+
+            it('should update current and longest streak to 4', function () {
+                const dates = [];
+                for (let i = 0; i < 4; i++) {
+                    let newDate = new Date();
+                    newDate.setDate(newDate.getDate() - i);
+                    dates.push(newDate);
+                }
+                dates.forEach(date => this.test.habit.addDoneAt(date));
+                this.test.habit.updateStreaks();
+                this.test.habit.currentStreak.should.be.equal(4);
+                this.test.habit.longestStreak.should.be.equal(4);
+            });
+
+        });
     });
 
+    describe('habit with only monday and wednesday', function () {
+
+        beforeEach(function () {
+            let startDate = new Date();
+            startDate.setDate(startDate.getDate() - 14);
+            const habit = new Habit({
+                "name": "test",
+                "weekdays": [0, 3],
+                "userId": new ObjectId(userId),
+                "startDate": startDate
+            });
+            this.currentTest.habit = habit;
+        });
+
+        describe('update streaks to 2', function () {
+
+            it('should update streak to 2', function () {
+                const dates = [];
+                let wednesdayDate = new Date();
+                while (wednesdayDate.getDay() !== 4) {
+                    wednesdayDate.setDate(wednesdayDate.getDate() - 1);
+                }
+                dates.push(wednesdayDate);
+                while (wednesdayDate.getDay() !== 1) {
+                    wednesdayDate.setDate(wednesdayDate.getDate() - 1);
+                }
+                dates.forEach(date => this.test.habit.addDoneAt(date));
+                this.test.habit.updateStreaks();
+                this.test.habit.currentStreak.should.be.equal(2);
+                this.test.habit.longestStreak.should.be.equal(2);
+            });
+        })
+    });
 }); 
--- server/src/test/services/testHabitService.js
+++ server/src/test/services/testHabitService.js
@@ -2,48 +2,74 @@ process.env.NODE_ENV = 'TEST';
 
 const chai = require('chai');
 const should = chai.should();
-const dateTime = require('chai-datetime'); 
+const dateTime = require('chai-datetime');
+var chaiAsPromised = require("chai-as-promised");
 const Habit = require('../../models/habit');
 const ObjectId = require('mongoose').Types.ObjectId;
 const db = require('mongoose');
 const { db_url } = require('../../config');
 const habitService = require('../../services/habitService');
 
-chai.use(dateTime); 
+chai.use(dateTime);
+chai.use(chaiAsPromised); 
 
 const userId = '5f65ec1e364ff1431fdd6a62';
 
 describe('habitService', function () {
 
-    beforeEach(async function () {
-        try {
-            await db.connect(db_url, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            });
-            const habit = new Habit({
-                "name": "test",
-                "weekdays": [0, 1, 2, 3, 4, 5, 6],
-                "userId": new ObjectId(userId), 
-                "startDate": new Date('Mon Sep 21 2020 13:31:16')
-            });
-            this.currentTest.habit = await habit.save();
-        } catch (error) {
-            console.error(error);
-        }
-    });
-
-    afterEach(async function () {
-        await Habit.deleteMany({});
-    });
-
     describe('updateHabit', function () {
 
-        describe('invalid', function () {
+        beforeEach(async function () {
+            try {
+                await db.connect(db_url, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                });
+                const habit = new Habit({
+                    "name": "test",
+                    "weekdays": [0],
+                    "userId": new ObjectId(userId),
+                    "startDate": new Date('Mon Sep 21 2020 13:31:16')
+                });
+                this.currentTest.habit = await habit.save();
+            } catch (error) {
+                console.error(error);
+            }
+        });
 
+        afterEach(async function () {
+            await Habit.deleteMany({});
+        });
+
+        describe('invalid', function () {
+            it('Should throw invalid weekday error', async function () {
+                habitService.updateHabit(this.test.habit, { "dates": [new Date('Wed Sep 23 2020 13:31:16 GMT+0200')] }).should.be.rejectedWith(Error);
+            });
         });
 
         describe('valid', function () {
+
+            beforeEach(async function () {
+                try {
+                    await db.connect(db_url, {
+                        useNewUrlParser: true,
+                        useUnifiedTopology: true
+                    });
+                    const habit = new Habit({
+                        "name": "test",
+                        "weekdays": [0, 1, 2, 3, 4, 5, 6],
+                        "userId": new ObjectId(userId),
+                        "startDate": new Date('Mon Sep 21 2020 13:31:16')
+                    });
+                    this.currentTest.habit = await habit.save();
+                } catch (error) {
+                    console.error(error);
+                }
+            });
+
+            afterEach(async function () {
+                await Habit.deleteMany({});
+            });
 
             it('should update name', async function () {
                 const updatedHabit = await habitService.updateHabit(this.test.habit, { "name": "updated name" });
@@ -52,7 +78,7 @@ describe('habitService', function () {
             });
 
             it('should update start date ', async function () {
-                const date = new Date('Thu Sep 24 2020 13:24:14 GMT+0200'); 
+                const date = new Date('Thu Sep 24 2020 13:24:14 GMT+0200');
                 const updatedHabit = await habitService.updateHabit(this.test.habit, { "startDate": date });
                 updatedHabit.startDate.should.be.equalTime(date);
 
@@ -66,15 +92,15 @@ describe('habitService', function () {
 
             it('should add reward ', async function () {
                 const updatedHabit = await habitService.updateHabit(this.test.habit, { "rewards": [{ "name": "reward", "requiredStreak": 2 }] });
-                updatedHabit.rewards.should.be.lengthOf(1); 
-                updatedHabit.rewards[0].name.should.be.equal('reward'); 
-                updatedHabit.rewards[0].requiredStreak.should.be.equal(2); 
+                updatedHabit.rewards.should.be.lengthOf(1);
+                updatedHabit.rewards[0].name.should.be.equal('reward');
+                updatedHabit.rewards[0].requiredStreak.should.be.equal(2);
             });
 
             it('should add date', async function () {
-                const dates = [new Date('Mon Sep 21 2020 13:31:16'), new Date('Wed Sep 23 2020 13:31:16 GMT+0200')]; 
+                const dates = [new Date('Mon Sep 21 2020 13:31:16'), new Date('Wed Sep 23 2020 13:31:16 GMT+0200')];
                 const updatedHabit = await habitService.updateHabit(this.test.habit, { "dates": dates });
-                updatedHabit.doneAtDates.should.be.lengthOf(2); 
+                updatedHabit.doneAtDates.should.be.lengthOf(2);
                 updatedHabit.doneAtDates[1].should.be.equalTime(new Date('Mon Sep 21 2020 00:00:00'));
                 updatedHabit.doneAtDates[0].should.be.equalTime(new Date('Wed Sep 23 2020 00:00:00'));
             });
