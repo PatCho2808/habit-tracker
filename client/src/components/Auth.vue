a49a1fc2b46afc85f2253b8e7d17d71b160f6c64
Patrycja <patcho2808@gmail.com>
2020-10-04 14:50:05 +0200

    Added AuthForm component

--- client/src/components/Auth.vue
+++ client/src/components/Auth.vue
@@ -1,68 +1,23 @@
 <template>
   <div class="row" id="auth">
-    <div class="col s12 m6">
-      <div class="card grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Log in</span>
-          <div class="row">
-            <form class="col s12">
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="username_login" type="text" class="validate" />
-                  <label for="username_login">Username</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="password_login" type="password" class="validate" />
-                  <label for="password_login">Password</label>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="card-action">
-            <a class="waves-effect waves-light btn">Log in</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m6">
-      <div class="card grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">Sign up</span>
-          <div class="row">
-            <form class="col s12">
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="username_signup" type="text" class="validate" />
-                  <label for="username_signup">Username</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="password_signup" type="password" class="validate" />
-                  <label for="password_signup">Password</label>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="card-action">
-            <a class="waves-effect waves-light btn">Sign up</a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <AuthForm action="Log in" />
+    <AuthForm action="Sign up" />
   </div>
 </template>
 
 <script>
+import AuthForm from './AuthForm.vue';
+
 export default {
   name: 'Auth',
+  components: {
+    AuthForm,
+  },
 };
 </script>
 
 <style scoped>
-#auth{
+#auth {
   padding: 30px;
 }
 </style>
---
+++ client/src/components/AuthForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col s12 m6">
+    <div class="card grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title">{{ action }}</span>
+        <div class="row">
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s12">
+                <input :id="'username_' + actionId" type="text" class="validate" />
+                <label :for="'username_' + actionId">Username</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input :id="'password_' + actionId" type="password" class="validate" />
+                <label :for="'password_' + actionId">Password</label>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="card-action">
+          <a class="waves-effect waves-light btn">{{ action }}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthForm',
+  props: {
+    action: String,
+  },
+  computed: {
+    actionId() {
+      return this.action.toLowerCase().replace(' ', '_');
+    },
+  },
+};
+</script>
