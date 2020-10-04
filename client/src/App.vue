0b18b1f89ee2be2b67d7ab51269921e9e1a83fd6
Patrycja <patcho2808@gmail.com>
2020-10-04 14:08:34 +0200

    Added auth component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <nav>
+      <div class="nav-wrapper purple darken-3">
+        <a id="logo" href="#" class="brand-logo">Habit tracker</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        </ul>
+      </div>
+    </nav>
+    <div id="container">
+      <Auth></Auth>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Auth from './components/Auth.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Auth,
   },
 })
+
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#logo {
+  padding-left: 40px;
 }
 </style>
---
+++ client/src/components/Auth.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="row" id="auth">
+    <div class="col s12 m6">
+      <div class="card grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Log in</span>
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="username_login" type="text" class="validate" />
+                  <label for="username_login">Username</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="password_login" type="password" class="validate" />
+                  <label for="password_login">Password</label>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="card-action">
+            <a class="waves-effect waves-light btn">Log in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6">
+      <div class="card grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Sign up</span>
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="username_signup" type="text" class="validate" />
+                  <label for="username_signup">Username</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="password_signup" type="password" class="validate" />
+                  <label for="password_signup">Password</label>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="card-action">
+            <a class="waves-effect waves-light btn">Sign up</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+};
+</script>
+
+<style scoped>
+#auth{
+  padding: 30px;
+}
+</style>
